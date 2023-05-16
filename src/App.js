90ecf75cb0774f889d565e51ac18b59648fbeb4b
SANJAIGOVINDRAJ <124117481+SANJAIGOVINDRAJ@users.noreply.github.com>
2023-05-16 21:35:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from './pages/Form'
+
+const App = () => {
+  return (
+    <div className=' '><Form /></div>
+  )
+}
+
+export default App
---
+++ src/pages/Form.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div classname="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div classname="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2 classname="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+  </div>
+
+  <div classname="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form classname="space-y-6" action="#" method="POST">
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div classname="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required classname="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        </div>
+      </div>
+
+      <div>
+        <div classname="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          <div classname="text-sm">
+            <a href="#" classname="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+          </div>
+        </div>
+        <div classname="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required classname="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" classname="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
+    </form>
+
+    <p classname="mt-10 text-center text-sm text-gray-500">
+      Not a member?
+      <a href="#" classname="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+    </p>
+  </div>
+</div>
+  );
+};
+
+export default Form;
